c28fc414b70682ade77e1b8a14620814ddc16355
Luke Wooten <wootenl@wit.edu>
2019-12-02 15:08:55 -0500

    Now has specific worker instructions based on number of woorkers. Request client communications now confirmed to work.

--- server.py
+++ server.py
@@ -9,36 +9,90 @@ serverSocket.bind((serverHost, serverPort))
 serverSocket.listen(10)
 print('The server is waiting on clients...')
 workers = list()
+workerHandlers = list()
 requester = 0
-passHash = 0
+passHash = ""
 
 def requestHandler(passHash, solved):
     passHash = connectionSocket.recv(1024).decode()
-
-
+    print(passHash)
 
 def workerHandler(connectionSocket, workers, passHash):
     connectionSocket.send(passHash.encode())
+    if len(workers) == 1:
+        parameters = "123456"
+        workers[0].send(parameters.encode())
+    elif len(workers) == 2:
+         parameters = "16"
+         workers[0].send(parameters.encode())
+         parameters = "2345"
+         workers[1].send(parameters.encode())
+    elif len(workers) == 3:
+         parameters = "6"
+         workers[0].send(parameters.encode())
+         parameters = "15"
+         workers[1].send(parameters.encode())
+         parameters = "234"
+         workers[2].send(parameters.encode())
+    elif len(workers) == 4:
+         parameters = "6"
+         workers[0].send(parameters.encode())
+         parameters = "5"
+         workers[1].send(parameters.encode())
+         parameters = "14"
+         workers[2].send(parameters.encode())
+         parameters = "23"
+         workers[3].send(parameters.encode())
+    elif len(workers) == 5:
+         parameters = "6"
+         workers[0].send(parameters.encode())
+         parameters = "5"
+         workers[1].send(parameters.encode())
+         parameters = "4"
+         workers[2].send(parameters.encode())
+         parameters = "3"
+         workers[3].send(parameters.encode())
+         parameters = "12"
+         workers[4].send(parameters.encode())
+    elif len(workers) == 6:
+         parameters = "6"
+         workers[0].send(parameters.encode())
+         parameters = "5"
+         workers[1].send(parameters.encode())
+         parameters = "4"
+         workers[2].send(parameters.encode())
+         parameters = "3"
+         workers[3].send(parameters.encode())
+         parameters = "2"
+         workers[4].send(parameters.encode())
+         parameters = "1"
+         workers[6].send(parameters.encode())
 
 while True:
      connectionSocket, addr = serverSocket.accept()
 
      print('Connection recieved from: ', addr)
-     query = ("Welcome to the password cracker! Enter '1' to request a password to crack, or '2' to contribute to cracking a password.")
-     
-     connectionSocket.send(query.encode())
      queryResponse = connectionSocket.recv(1024).decode()
 
      if queryResponse == "1":
          if requester == 0:
+             print(addr, " Is a requester.")
              requestHandler(passHash, "no")
          else:
              connectionSocket.send("Currently handling a differnet request.".encode())
+             connectionSocket.close()
      elif queryResponse == "2":
-         workerHandler(connectionSocket, workers, passHash)
-         workers.append(connectionSocket)
-         thread = threading.Thread(target=workerHandler, args=(connectionSocket, workers, passHash), daemon=True)
-         thread.start()
+         if len(workers) == 6:
+             maxMessage = "Number of workers at capacity."
+             connectionSocket.send(maxMessage.encode())
+             connectionSocket.close()
+         else: 
+             print(addr, " Is a worker.")
+             workerHandler(connectionSocket, workers, passHash)
+             workers.append(connectionSocket)
+             thread = threading.Thread(target=workerHandler, args=(connectionSocket, workers, passHash), daemon=True)
+             workerHandlers.append(thread)
+             thread.start()
      else:
          invalidMessage = ("Server did not recognize response.")
          connectionSocket.send(invalidMessage.encode())
